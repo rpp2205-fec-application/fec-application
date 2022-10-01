1432c385299b7c3c9974a1e44e5aec6e5a6f8743
Cameron Hirsh <camjhirsh@gmail.com>
2022-10-01 13:16:28 -0700

    added QA section

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const path = require('path');
 const axios = require('axios');
-const express = require('express'); // npm installed
+const express = require('express');
 const app = express();
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
-// other configuration...
+
 app.get('/review', (req, res) => {
   let url = "http://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/?product_id=71697";
   return axios.get(url, {headers: {authorization: process.env.TOKEN}})
@@ -15,8 +16,10 @@ app.get('/review', (req, res) => {
     })
 })
 
-
+// app.get('/QA', (req, res) => {
+//   axios.get()
+// })
 
 
 let PORT = process.env.PORT || 3000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening at Port: ${PORT}`));
