f62560d3508eec83e892381c896c561baa88b2d0
Rain <pyc0422@gmail.com>
2022-10-04 00:31:54 -0500

    finished convert rating to star

--- client/src/components/reviews/ReviewEntry.jsx
+++ client/src/components/reviews/ReviewEntry.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { format, parseJSON } from 'date-fns';
+import Star from '../getReviews/Star.jsx';
 const ReviewEntry = (props) => {
   return (
     <li>
       <div className="rev">
         <div className="rev-header">
-          <div className="star">{props.review.rating}</div>
+          <Star rating={props.review.rating} />
           <div className="date small_font">{props.review.reviewer_name}, {format(parseJSON(props.review.date), "MMMM/dd/yyyy")}</div>
         </div>
         <div className="rev-body">
@@ -13,7 +14,7 @@ const ReviewEntry = (props) => {
           <div className="review small_font">{props.review.body}</div>
         </div>
         <div className="rev-footer small_font">
-          <p>Helpful?Yes. | Report &#9733;</p>
+          <p>Helpful?Yes. | Report </p>
           <hr/>
         </div>
       </div>
