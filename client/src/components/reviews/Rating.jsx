124089f653d6c6e574e8259ea078ac6baf9f55bf
Rain <pyc0422@gmail.com>
2022-10-05 23:57:12 -0500

    handled reviews percentage progress

--- client/src/components/reviews/Rating.jsx
+++ client/src/components/reviews/Rating.jsx
@@ -5,95 +5,31 @@ import {calculateRating, roundNearQtr} from '../../helpers.js';
 const Rating = (props) => {
   const [rating, setRating] = useState(props.rating);
   const [reviewsMeta, setMeta] = useState(props.reviewsMeta);
-  console.log('inside rating: ', reviewsMeta.recommended);
   const recommend = parseInt(reviewsMeta.recommended.true) / (parseInt(reviewsMeta.recommended.true) + parseInt(reviewsMeta.recommended.false));
-  console.log('recommend: ', recommend);
+  const totalRating = Object.values(reviewsMeta.ratings).reduce((acc, n) => {
+    return acc = acc + parseInt(n);
+  }, 0)
+  console.log('total: ', totalRating);
   return (
     <div className="breakdown">
       <div className="rat-header">
         <div className="xxl_font">{rating}</div>
         <div className="stars"><Star rating={roundNearQtr(rating)}/></div>
       </div>
-      <div>{recommend * 100}% of reviews recommend this product</div>
-      <div className="rav-body">
-        <div className="rev-chart">
-          <div className="rev-stars"><span>5 Stars</span></div>
-          <div className="rev-progress"></div>
-        </div>
-        <div className="rev-chart">
-          <div className="rev-stars"><span>4 Stars</span></div>
-          <div className="rev-progress"></div>
-        </div>
-        <div className="rev-chart">
-          <div className="rev-stars"><span>3 Stars</span></div>
-          <div className="rev-progress"></div>
-        </div>
-        <div className="rev-chart">
-          <div className="rev-stars"><span>2 Stars</span></div>
-          <div className="rev-progress"></div>
-        </div>
-        <div className="rev-chart">
-          <div className="rev-stars"><span>1 Stars</span></div>
-          <div className="rev-progress"></div>
-        </div>
+      <div className="xs_font">{recommend * 100}% of reviews recommend this product</div>
+      <div className="rat-body xs_font">
+        {Object.keys(reviewsMeta.ratings).reverse().map(key =>
+            <div className="rat-chart" key={key}>
+              <div className="rat-stars">
+                <span className="underline">{key} stars</span>
+                <span className="rat-progress" style={{"--stars": parseInt(reviewsMeta.ratings[key]) / totalRating}}></span>
+              </div>
+            </div>
+        )}
       </div>
     </div>
-
   )
 }
-// class Rating extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       avg_rating: this.props.rating,
-//       click: ''
-//     }
-//     // this.avg_rating;
-//   }
 
-//   render() {
-//     // if (this.props.reviews.length) {
-//     //   this.avg_rating = calculateRating(this.props.reviews);
-//     // }
-
-//     return (
-//       <div className="breakdown">
-//           {this.props.rating ?
-//           <div className="rat-header">
-//             <div className="xxl_font">{this.props.rating}</div>
-//             <div className="stars"><Star rating={roundNearQtr(this.props.rating)}/></div>
-//           </div>
-//           : null}
-
-
-//         <div>% of reviews recommend this product</div>
-//         <div className="rav-body">
-//           <div className="rev-chart">
-//             <div className="rev-stars"><span>5 Stars</span></div>
-//             <div className="rev-progress"></div>
-//           </div>
-//           <div className="rev-chart">
-//             <div className="rev-stars"><span>4 Stars</span></div>
-//             <div className="rev-progress"></div>
-//           </div>
-//           <div className="rev-chart">
-//             <div className="rev-stars"><span>3 Stars</span></div>
-//             <div className="rev-progress"></div>
-//           </div>
-//           <div className="rev-chart">
-//             <div className="rev-stars"><span>2 Stars</span></div>
-//             <div className="rev-progress"></div>
-//           </div>
-//           <div className="rev-chart">
-//             <div className="rev-stars"><span>1 Stars</span></div>
-//             <div className="rev-progress"></div>
-//           </div>
-//         </div>
-//       </div>
-
-
-//     )
-//   }
-// }
 
 export default Rating;
