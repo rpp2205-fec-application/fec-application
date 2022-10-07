48b89933c212b58607b2a5163bba970a635bab85
Cameron Hirsh <camjhirsh@gmail.com>
2022-10-06 17:22:51 -0700

    some styling

--- client/src/components/qa/Answer.jsx
+++ client/src/components/qa/Answer.jsx
@@ -4,7 +4,12 @@ const Answer = (props) => {
   return (
     <div className="answer-section">
       <p className="questionBody"> A: </p>
-      <p className="answerBody"> {props.answer.body} </p>
+      <div className="answer">
+        <p className="answer-body"> {props.answer.body} </p>
+        <p> by User: username Date </p>
+        <p> Helpful? Yes (0) </p>
+        <p> Report </p>
+      </div>
     </div>
   )
 }
