f1fde8cd94889fba019004617af9393c02de9298
viviennema <98420335+viviennema@users.noreply.github.com>
2022-10-08 11:19:07 -0700

    updated new Progress

--- client/src/components/relatedItems/SingleCard.jsx
+++ client/src/components/relatedItems/SingleCard.jsx
@@ -14,14 +14,15 @@ class SingleCard extends React.Component {
   componentDidMount() {
     axios.get(`/products/${this.props.product}`)
       .then(response => {
-        // console.log('response///', response.data);
+        console.log('response///', response.data);
         this.setState({
           product: response.data
         })
       })
       axios.get(`/products/${this.props.product}/styles`)
       .then(response => {
-        // console.log('styles///', response.data);
+         console.log('styles///', response.data);
+         console.log('photos///', response.data.results[0].photos[0].thumbnail_url);
         this.setState({
           productStyle: response.data
         })
@@ -32,19 +33,21 @@ class SingleCard extends React.Component {
   render() {
     return (
       <div className='slide-container'>
-      <div className='slide-content'>
-        <div className='card-wrapper'>
-          <div className='card'>
-           <div className='image-content'>
-            <span className='overlay' />
-            <div className="card-image">
-              <img className='card-img'/>
+        <div className="card">
+          <div className='card-image'>
+            <a>
+            <img src={this.state?.productStyle?.results?.[0]?.photos?.[0]?.thumbnail_url} />
+            </a>
+            <div className='cardbody'>
+            <p className='category'>{this.state.product.category}</p>
+            <h4 className='name'>{this.state.product.name}</h4>
+            <p className='price'>{this.state.product.default_price}</p>
+            <p>ratings</p>
             </div>
-           </div>
           </div>
         </div>
       </div>
- </div>
+    
     )
   }
 
