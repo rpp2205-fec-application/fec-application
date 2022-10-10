0cbe9067fe627e0db605cf0e0c4d9088c0ab805b
Rain <pyc0422@gmail.com>
2022-10-10 16:54:10 -0500

    end of Monday afternoon already handled modal and sub-modal.Still working on the add review form content and style

--- client/src/components/reviews/AddReview.jsx
+++ client/src/components/reviews/AddReview.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import UploadPics from './UploadPics.jsx';
 const AddReview = (props) => {
   const [newRev, setRev] = useState({
     product_id: props.product.id,
@@ -12,7 +12,21 @@ const AddReview = (props) => {
     photos: [],
     characteristics: {}
   })
+  const [uploadShow, setUploadShow] = useState(false);
   const showOrHide = props.show ? "modal display-block" : "modal display-none";
+  const handleUpload = () => {
+    setUploadShow(!uploadShow);
+  }
+  const handleRadio = (e) => {
+    let value = e.target.value;
+    if (value === "yes") {
+      setRev({...newRev, recommend: true})
+    } else {
+      setRev({...newRev, recommend: false})
+    }
+
+  }
+
   return (
     <div className={showOrHide}>
       <div className="modal_content">
@@ -26,8 +40,8 @@ const AddReview = (props) => {
           <label>show rating star</label>
         </div>
         <div>Recommond?
-          <label><input type="radio" value="yes" checked={true} />Yes</label>
-          <label><input type="radio" value="no" />No</label>
+          <label><input type="radio" value="yes" onChange={(e) => {handleRadio(e)}} checked={newRev.recommend} />Yes</label>
+          <label><input type="radio" value="no" onChange={(e) => {handleRadio(e)}} checked={!newRev.recommend} />No</label>
         </div>
         <div>
           <label>Characteristics</label>
@@ -39,11 +53,12 @@ const AddReview = (props) => {
           <label>Review Body: <input type="text" placeholder="no more then 1000 chars"></input></label>
         </div>
         <div>
-          <label>Upload your photos</label>
+          <UploadPics show={uploadShow} handleClicked={handleUpload}/>
+          <button onClick={handleUpload}>Upload your photos</button>
         </div>
         <div>
-          <label>Nick Name:  <input type="text" placeholder="Your nick name"/></label>
-          <label>Your Email:  <input type="text" placeholder="xxx@gmail.com"/></label>
+          <label>Nick Name:  <input type="text" placeholder="Type nick name"/></label>
+          <label>Your Email:  <input type="email" placeholder="xxx@gmail.com"/></label>
         </div>
         <div>
           <input className="btn" type="submit" value="Submit"/>
