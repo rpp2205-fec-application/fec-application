53a886d8e3f6d3ed4bf4d4f9ed9cbc49df4d31f7
Rain <pyc0422@gmail.com>
2022-10-11 22:47:03 -0500

    handled length of summary and body, add a SHOWMORE btn

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -40,7 +40,7 @@ class App extends React.Component {
       .then(res => {
         return this.setState({
           products: res.data,
-          product: res.data[0]
+          product: res.data[14]
         }, () => {
           console.log('Products: ', this.state.products)
           console.log('Product: ', this.state.product)
