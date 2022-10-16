135eb31e01978656e7a90f26cb9494aaf96e831e
Cameron Hirsh <camjhirsh@gmail.com>
2022-10-16 14:29:06 -0700

    styling search bar

--- client/src/components/qa/QuestionBar.jsx
+++ client/src/components/qa/QuestionBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaSistrix } from "react-icons/fa";
 
 class QuestionBar extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class QuestionBar extends React.Component {
     return (
       <div className="searchSection">
         <input className="searchBar" type="search" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." onChange={this.handleChange.bind(this)}/>
+        <FaSistrix className="mag" />
       </div>
     )
   }
