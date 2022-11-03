2cdd0bd4566cc9b621b508835e90f25ae450e4e3
Rain <pyc0422@gmail.com>
2022-11-02 22:35:12 -0500

    create config.example for developer

---
+++ config.example.js
@@ -0,0 +1,10 @@
+//accessing atelier API
+PORT = "Your port"
+TOKEN = "Your github token"
+// accessing image hosting cloudinary
+CLOUDINARY_URL="your cloudinary url"
+CLOUD_NAME="your cloudinary name"
+CLOUD_KEY="your cloudinary key"
+CLOUD_SECRET="your cloudinary secret"
+//copy this file and fill your value of these property
+//then change the file name to '.env'
